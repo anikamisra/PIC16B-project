559312ce55d53e4d77cb52fa22d242665fa06c8b
Athena Mo <50725534+Fishier1224@users.noreply.github.com>
2024-02-12 12:27:23 -0800

    Implement fake useragent for scraping

--- 16bProject/ChordsScraper/ChordsScraper/settings.py
+++ 16bProject/ChordsScraper/ChordsScraper/settings.py
@@ -7,6 +7,29 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+CLOSESPIDER_PAGECOUNT = 10
+
+SCRAPEOPS_API_KEY = '89c1ce85-664d-4d93-99b4-93bee4be79a0'
+SCRAPEOPS_PROXY_ENABLED = True
+
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+    'scrapy_fake_useragent.middleware.RetryUserAgentMiddleware': 401,
+}
+
+
+FAKEUSERAGENT_PROVIDERS = [
+    'scrapy_fake_useragent.providers.FakeUserAgentProvider',  # This is the first provider we'll try
+    'scrapy_fake_useragent.providers.FakerProvider',  # If FakeUserAgentProvider fails, we'll use faker to generate a user-agent string for us
+    'scrapy_fake_useragent.providers.FixedUserAgentProvider',  # Fall back to USER_AGENT value
+]
+
+## Set Fallback User-Agent
+USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+
 BOT_NAME = "ChordsScraper"
 
 SPIDER_MODULES = ["ChordsScraper.spiders"]
