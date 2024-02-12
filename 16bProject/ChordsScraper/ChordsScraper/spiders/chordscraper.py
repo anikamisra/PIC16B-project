c9a181d90e66e24a8784fbd3ef7491ca1db9710b
Athena Mo <50725534+Fishier1224@users.noreply.github.com>
2024-02-12 12:41:04 -0800

    Update chordscraper.py

--- 16bProject/ChordsScraper/ChordsScraper/spiders/chordscraper.py
+++ 16bProject/ChordsScraper/ChordsScraper/spiders/chordscraper.py
@@ -1,2 +1,14 @@
 import scrapy
 
+class ChordsScraperSpider(scrapy.Spider):
+    name = 'ChordsScraper'
+
+    def __init__(self, *args, **kwargs):
+        self.start_urls = [f"https://www.ultimate-guitar.com/explore"]
+
+    def parse(self, response):
+        """
+        Function assumes that we are on the home page of website
+        Gets individual links for songs
+        """
+        return(0)
