f16da821299ad33188bf577cdb842acfa8f8d5b2
Isabella Woulfe <156366973+woulfeig@users.noreply.github.com>
2024-02-13 08:46:16 -0800

    Update chordscraper.py
    this is what I made in my VS code and put on our slides

--- 16bProject/ChordsScraper/ChordsScraper/spiders/chordscraper.py
+++ 16bProject/ChordsScraper/ChordsScraper/spiders/chordscraper.py
@@ -1,14 +1,14 @@
 import scrapy
 
 class ChordsScraperSpider(scrapy.Spider):
-    name = 'ChordsScraper'
+    name = 'chord_scraper'
 
-    def __init__(self, *args, **kwargs):
-        self.start_urls = [f"https://www.ultimate-guitar.com/explore"]
-
-    def parse(self, response):
-        """
-        Function assumes that we are on the home page of website
-        Gets individual links for songs
-        """
-        return(0)
+    def __init__(self, subdir=None, *args, **kwargs):
+        self.start_urls = [f"http://www.mldb.org/song-250991-dance-the-night.html"]
+        
+        
+    def parse_song_title(self, response):
+        song_title = response.css('h1::text').extract()
+        song_artist = response.css('a::text').extract()[51]
+        song_lyrics = response.css('p.songtext::text').extract() 
+        yield {"song" : song_title, "artist" : song_artist, "lyrics" : song_lyrics}
