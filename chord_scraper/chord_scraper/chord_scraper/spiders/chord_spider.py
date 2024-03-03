b268f5c652f30627cd66df5463483c818c3f6fa4
Anika Misra <anikamisra@Anikas-MBP.lan>
2024-03-02 22:16:46 -0800

    added parse artist page to scraper

--- chord_scraper/chord_scraper/chord_scraper/spiders/chord_spider.py
+++ chord_scraper/chord_scraper/chord_scraper/spiders/chord_spider.py
@@ -3,7 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from scrapy.selector import Selector
 from scrapy.http import Request
+import time 
 
+# this code parses the song page once you are on it. 
+# HERE IS WHAT TO PUT IN THE TERMINAL: 
+# if you are using the first scraper: 
+# scrapy crawl chord_scraper -o results.csv -a url_of_song=https://chordify.net/chords/ariana-grande-songs/dangerous-woman-chords
+
+# if you are using the second scraper: 
+# scrapy crawl chord_scraper -o results.csv -a artistname=dua-lipa
+"""
 class chord_scraper(scrapy.Spider): 
     name = 'chord_scraper'
     
@@ -39,4 +48,52 @@ class chord_scraper(scrapy.Spider):
             yield row
 
     def closed(self, reason):
+        self.driver.quit()"""
+
+# this code parses the artist page and returns the list of songs for a given artist. 
+
+class chord_scraper(scrapy.Spider): 
+    name = 'chord_scraper'
+    
+    def __init__(self, artistname, *args, **kwargs): 
+        super().__init__(*args, **kwargs)  
+        # format artist name properly 
+        artistname_formatted = ("-".join(artistname.split())).lower()
+        
+        # form url of artist page 
+        artist_page_url = "https://chordify.net/chords/"+artistname_formatted+"-songs"
+        # need to add an error catcher for if this artist page doesn't exist 
+        self.start_urls = [artist_page_url]
+        
+        options = Options()
+        options.headless = True
+        self.driver = webdriver.Firefox(options=options)
+    def parse(self, response):
+        
+        self.driver.get(response.url) 
+        time.sleep(5)
+        html = self.driver.page_source
+        sel = Selector(text=html) 
+        # select the main div containing all the songs
+        main_div = sel.css('div.s1qyqb8i.g1aau9lx')
+        
+        for link in sel.css('div.s1qyqb8i.g1aau9lx a::attr(href)'):
+        # get the url
+            song_url = link.get()
+
+        # get the associated song title
+            #song_title_parts = link.xpath('./following-sibling::div[1]/span/text()').getall()
+            #song_title = ''.join(song_title_parts)
+            start = song_url.rfind("/")
+            end = len(song_url) - 7 
+            song_title = song_url[start+1:end]
+            song_title = (song_title.replace('-', ' ')).title()
+
+        # yield the result as a dictionary
+            yield {
+                'song_name': song_title,
+                'song_url': song_url,
+            }
+        
+    def closed(self, reason): 
         self.driver.quit()
