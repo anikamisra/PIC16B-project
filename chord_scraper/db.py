a1fa06c6a85a6e3507eb1f9801f2f662c07ae657
Athena Mo <Athena_1224@outlook.com>
2024-03-21 21:23:14 -0700

    Added docstring for db.py

--- chord_scraper/db.py
+++ chord_scraper/db.py
@@ -3,12 +3,29 @@ from pymongo.server_api import ServerApi
 from pymongo.errors import InvalidOperation
 
 class User:
+    """
+    Represents a user with a username and password.
+    """
     def __init__(self, username, password):
+        """
+        Initializes a User object with the provided username and password.
+        Args:
+            username (str): The username of the user.
+            password (str): The password of the user.
+        """
         self.username = username
         self.password = password
 
 class Database:
+    """
+    Represents a MongoDB database connection.
+    """
     def __init__(self, db_pswd):
+        """
+        Initializes a Database object with the provided MongoDB password.
+        Args:
+            db_pswd (str): The MongoDB password.
+        """
         uri = f"mongodb+srv://Chordy:{db_pswd}@cluster0.pirmgae.mongodb.net/"
         print(uri)
         self.client = MongoClient(uri, server_api=ServerApi('1'))
@@ -21,6 +38,13 @@ class Database:
             print(e)
 
     def add_user(self, user: User):
+        """
+        Adds a new user to the MongoDB database.
+        Args:
+            user (User): The User object to be added to the database.
+        Returns:
+            bool: True if the user is successfully added, False otherwise.
+        """
         if self.db.users.find_one({"username": user.username}) is not None:
             return False
         else:
@@ -28,6 +52,14 @@ class Database:
             return True
 
     def verify_user(self, username, password):
+        """
+        Verifies the credentials of a user.
+        Args:
+            username (str): The username to be verified.
+            password (str): The password to be verified.
+        Returns:
+            bool: True if the credentials are valid, False otherwise.
+        """
         user = self.db.users.find_one({"username": username})
         if user:
             if user["password"] == password:
